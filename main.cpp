e730714ca59384487c40a5fc45610b4bc84d3f26
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2025-08-18 17:45:42 +0200

    add command line arguments

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 // Copyright (C) 2025 Pedro López-Cabanillas
-// Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setApplicationName(QT_STRINGIFY(PROGRAM));
+    QCoreApplication::setApplicationVersion(QT_STRINGIFY(VERSION));
+
     QApplication app(argc, argv);
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "RIFF file");
+    parser.process(app);
+    // Retrieve command line arguments from Qt and parse options
+    QStringList args = parser.positionalArguments();
+
     MainWindow mainwin;
     mainwin.show();
+    if (args.size() > 0) {
+        mainwin.openFile(args.first());
+    }
     return QCoreApplication::exec();
 }
